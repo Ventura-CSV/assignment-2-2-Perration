4290bf1b102d81d1f2c01ef06d3f093a8f58baec
Perration <yolandag2013@yahoo.com>
2023-08-26 19:20:34 -0700

    commit

--- main.py
+++ main.py
@@ -3,8 +3,15 @@ def main():
     ##################################################
     # Comlete your code here
     Use the same variables: celcius fahrenheit 
-    ##################################################
+    ################################################## 
     """
+    #celcius23
+    
+    celcius = int(input("Enter tempeature in celcius: "))
+    
+    fahrenheit = 9/5 * celcius + 32
+    
+    print(f'Fahrenheit temperature: \t {fahrenheit:.2f}')
 
     """
     ########################################
